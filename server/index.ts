e91ae223460a8630085f1a7ec3166064b48a94f0
pedro823 <grey.pedrinho@gmail.com>
2019-07-20 00:15:20 +0100

    Adds webpage that spawns python shell

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 import http from 'http'
 import io from 'socket.io'
+import * as nodepty from 'node-pty'
 
 const expressApp = express()
 const server = http.createServer(expressApp)
@@ -17,9 +18,25 @@ server.listen(port, () => {
 })
 
 ioServer.on('connection', (socket: io.Socket) => {
+    const seedship = nodepty.spawn('/usr/bin/python3', [], {
+        name: 'xterm-color',
+        cwd: process.env.PWD,
+    })
+
+    seedship.on('data', (data) => {
+        socket.emit('write', data)
+    })
+
+    socket.on('command', (data) => {
+        seedship.write(data)
+    })
+    
+    console.log(seedship)
     console.log('an user connected')
 
+
     socket.on('disconnect', () => {
-        console.log('an user disconnected')
+        console.log('an user disconnected, seedship was killed.')
+        seedship.kill()
     })
 })
--- webclient/main.js
+++ webclient/main.js
@@ -1,1 +1,18 @@
-let socket = io();
+(function() {
+    Terminal.applyAddon(fullscreen);
+    Terminal.applyAddon(attach);
+    Terminal.applyAddon(search);
+
+    const term = new Terminal();
+    const container = document.getElementById('terminal');
+    term.open(container);
+    const socket = io();
+
+    term.on('data', (data) => {
+        socket.emit('command', data);
+    })
+
+    socket.on('write', (data) => {
+        term.write(data);
+    })
+})()
