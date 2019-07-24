3343637e5e9bf3112cd2341c8aa76ec7046001b3
pedro823 <grey.pedrinho@gmail.com>
2019-07-24 21:23:52 +0100

    Adds localhost to list of IPs if none are found elsewhere

--- server/utils.ts
+++ server/utils.ts
@@ -8,8 +8,9 @@ export const getNetworkIPs = () => {
     // Figures out what IP the host is
     const interfaces = os.networkInterfaces()
 
-    return flatMap(Object.values(interfaces), (interfaceList => 
+    const networkInterfaces = flatMap(Object.values(interfaces), interfaceList =>
         interfaceList.filter(iface => !iface.internal && iface.family === 'IPv4')
                      .map(iface => iface.address)
-    ))
+    )
+    return networkInterfaces.length > 0 ? networkInterfaces : ['localhost']
 }
