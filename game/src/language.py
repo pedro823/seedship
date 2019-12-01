9ba7330f45908561e29acb3b95a5d027b19a7a2f
pedro823 <grey.pedrinho@gmail.com>
2019-12-01 15:30:07 -0300

    small fix: allows program to be terminated on language choice by using EOF

--- game/src/language.py
+++ game/src/language.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 class LanguageError(Exception):
     pass
@@ -38,6 +39,8 @@ class Language:
                     self.language = self.AVAIL_LANGS[language_choice]
                     self.txt = self.load_language(self.language)
                     break
+            except EOFError:
+                sys.exit(1)
             except:
                 continue
 
